96f740a4461e3c3c3a875fe536ee195af05dfb5b
Bar Azenkot <barazenkot3@gmail.com>
2021-12-01 13:34:09 +0200

    modified signup controller

--- api/controllers/users.js
+++ api/controllers/users.js
@@ -1,15 +1,53 @@
-const mongoose = require("mongoose");
 const User = require("../models/user");
+const bcrypt = require("bcrypt");
+const mongoose = require("mongoose");
 
 module.exports = {
   signup: (req, res) => {
-    res.status(200).json({
-      message: "Sign Up",
+    const { email, password, username, fullName } = req.body;
+
+    User.find({ email }).then((users) => {
+      if (users.length > 0) {
+        return res.status(409).json({
+          message: "Email already in use",
+        });
+      }
+      bcrypt.hash(password, 10, (error, hash) => {
+        if (error) {
+          return res.status(500).json({ error });
+        }
+
+        const user = new User({
+          _id: new mongoose.Types.ObjectId(),
+          email,
+          password: hash,
+          username,
+          fullName,
+        });
+
+        user
+          .save()
+          .then((result) => {
+            res.status(200).json({
+              message: "User created successfully",
+            });
+          })
+          .catch((error) => {
+            res.status(500).json({ error });
+          });
+      });
     });
   },
   login: (req, res) => {
-    res.status(200).json({
-      message: "Login",
+    const { email, password } = req.body;
+    User.find({ email }).then((users) => {
+      if (users.length === 0) {
+        res.status(401).json({
+          message: "Auth failed",
+        });
+        // const [user] = users
+        // user[0].
+      }
     });
   },
 };
--- api/models/user.js
+++ api/models/user.js
@@ -4,7 +4,7 @@ require("mongoose-type-email");
 const userSchema = mongoose.Schema({
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  email: { type: mongoose.SchemaTypes.Email, required: true },
+  email: { type: mongoose.SchemaTypes.Email, required: true, unique: true },
   fullName: { type: String, required: true },
   bids: [{ type: mongoose.Schema.Types.ObjectId, required: false, ref: "Bid" }],
   auctions: [
